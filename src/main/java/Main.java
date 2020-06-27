53df758d3731f4033c2bac12d20f5f0bea6a3d6d
favael <rafaldogoda86@gmail.com>
2020-06-27 10:52:59 +0200

    pc added changes from mac

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,15 +1,34 @@
 import api.TibiaApi;
-import org.hibernate.SessionFactory;
 
+import org.hibernate.SessionFactory;
 import java.io.IOException;
 
 public class Main {
+
     private static SessionFactory factory;
     public static void main (String[] args) throws IOException, InterruptedException {
-        TibiaApi tibiaApi = new TibiaApi();
+
+    TibiaApi tibiaApi = new TibiaApi();
 
         String world = "Damora";
         tibiaApi.getHighcoreStat(world);
+
+
+//
+//        SessionProvider sessionProvider = SessionProvider.INSTANCE;
+//        SimpleStatsDAO dao = new SimpleStatsDAO(sessionProvider.getSession());
+//
+//
+//
+//
+//
+//        List<SimpleStats> dane = dao.getAll();
+//        for (SimpleStats stats : dane) {
+//            System.out.println(stats);
+//        }
+//        dao.close();
+//        sessionProvider.closeFactory();
+//
 
 
     }
